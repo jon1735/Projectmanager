3d652f9d3960fa148af8fc74a83d96480433d597
jon1735 <jonathan.gonzalez@powerhrg.com>
2021-10-20 11:24:31 -0400

    updating controllers for contractor, realtor, and property. Also added view and index files for all three

--- app/controllers/realtors_controller.rb
+++ app/controllers/realtors_controller.rb
@@ -1,2 +1,9 @@
 class RealtorsController < ApplicationController
+  def index
+    @realtors = Realtor.all
+  end
+
+  def show
+    @realtor = Realtor.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 
 Realtor.destroy_all
 Contractor.destroy_all
+Property.destroy_all
 
 Realtor.create({ name: "Chiodo Team Realty" })
 Realtor.create({ name: "Apex Realty" })
@@ -22,9 +23,8 @@ Contractor.create({ name: "Gallo Property Services", lawn_care: false, plumbing:
 Contractor.create({ name: "Guardian Home Care", lawn_care: false, plumbing: false, electrical: true })
 Contractor.create({ name: "Royalty Homes", lawn_care: true, plumbing: false, electrical: true })
 
-Property.create({ address: "1314 Astor Street, Norristown, PA", vacancy: false, lawn_care: "N/A", plumbing: "N/A", electrical: "N/A" })
-Property.create({ address: "22189 Chain Street, Philadelphia, PA", vacancy: true, lawn_care: "Lawn has not been maintained in 30 days, needs to be cut", plumbing: "Plumbing works fine", electrical: "Lights work fine" })
-Property.create({ address: "814 Green Valley Drive, Philadelphia, PA", vacancy: true, lawn_care: "Lawn recently cut, no work needed", plumbing: "Leaky kitchen faucet, need to replace ASAP", electrical: "Basement lights do not work" })
-Property.create({ address: "2501 Seaport Drive, Chester, PA", vacancy: true, lawn_care: "Lawn has not been maintained in 60 days, need full maintenance team", plumbing: "Foul odor coming out of 6th floor bathroom, need to inspect", electrical: "Lights work as intended" })
-Property.create({ address: "420 South High Street, West Chester, PA", vacancy: true, lawn_care: "Property does not have a lawn, no work needed here", plumbing: "3/5 toilets do not work and need to be replaced", electrical: "Lights work as intended" })
-Property.create({ address: "1776 Freedom Drive, Lancaster, PA", vacancy: true, lawn_care: "Lawn was recently cut, no work needed", plumbing: "Plumbing works fine, no work needed", electrical: "Lights work fine, no work needed" })
+Property.create!({ address: "1314 Astor Street, Norristown, PA", vacancy: false, lawn_care: "N/A", plumbing: "N/A", electrical: "N/A", realtor: Realtor.first, contractor: Contractor.first })
+Property.create({ address: "22189 Chain Street, Philadelphia, PA", vacancy: true, lawn_care: "Lawn has not been maintained in 30 days, needs to be cut", plumbing: "Plumbing works fine", electrical: "Lights work fine", realtor: Realtor.second, contractor: Contractor.second })
+Property.create({ address: "814 Green Valley Drive, Philadelphia, PA", vacancy: true, lawn_care: "Lawn recently cut, no work needed", plumbing: "Leaky kitchen faucet, need to replace ASAP", electrical: "Basement lights do not work", realtor: Realtor.third, contractor: Contractor.third })
+Property.create({ address: "2501 Seaport Drive, Chester, PA", vacancy: true, lawn_care: "Lawn has not been maintained in 60 days, need full maintenance team", plumbing: "Foul odor coming out of 6th floor bathroom, need to inspect", electrical: "Lights work as intended", realtor: Realtor.fourth, contractor: Contractor.fourth })
+Property.create({ address: "420 South High Street, West Chester, PA", vacancy: true, lawn_care: "Property does not have a lawn, no work needed here", plumbing: "3/5 toilets do not work and need to be replaced", electrical: "Lights work as intended", realtor: Realtor.fifth, contractor: Contractor.fifth })
