762dd63f6dffcfec7f070d98c68bd98c979f30d3
jon1735 <jonathan.gonzalez@powerhrg.com>
2021-10-21 14:22:42 -0400

    adding devise login/auth functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :properties
   resources :contractors
   resources :realtors
   resources :homes
+
+  root to: "homes#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
